be80463ef7cd4bac4f3a11ce4f4d909fce9e813e
DaNaCampean <campean.dana@gmail.com>
2025-05-30 18:25:12 +0300

    commit wind-28mai

---
+++ src/AlarmProblem.java
@@ -0,0 +1,103 @@
+public class AlarmProblem {
+}
+
+/*
+
+CX-T1557 - Dark Mode: Permission for customers associated with dealer 1/1115
+1 Step Login to customer site
+
+Test Data
+Use account under dealer 1411 in TEST (svAuto_darkModeVirtual/Password1!)
+
+Expected Result
+Verify logged into customer site
+
+2Step Navigate to Settings --> Login Information
+
+Expected Result
+Verify page loads
+
+3Step Look at options under “Login Information”
+
+Expected Result
+Verify “Site Appearance” is listed as an option
+
+
+CX-T1556 (1.0)Dark Mode: Toggling Dark Mode ON/OFF (Functional)
+
+1Step Login to customer site
+
+Test Data
+Use account under dealer 1411 in TEST (svAuto_darkModeVirtual/Password1!)
+
+Expected Result
+Verify logged into customer site
+
+2Step Click Settings in left nav menu
+
+Expected Result
+Verify Settings page loads
+
+3Step Click “Login Information” and then click “Site Appearance”
+
+Expected Result
+Verify Site Appearance page loads and displays 2 toggle buttons (one for dark mode and one for high contrast)
+4Step Click toggle for Dark Mode to on/enabled
+
+Expected Result
+Verify toggle UI change (should turn blue)
+
+5Step Click toggle for Dark Mode to off/disabled
+
+Expected Result
+Verify toggle UI change (color should change to black)
+
+
+CX-T1549 (1.0)Dark Mode: Saving DM/HC settings (Functional)Type: Step-by-Step
+Steps
+
+1Step Login to customer site
+
+Test Data
+Use account under dealer 1411 in TEST (svAuto_darkModeVirtual/Password1!)
+
+Expected Result
+Verify logged into customer site
+
+2Step Go to “Settings” and click “Login Information”
+
+Expected Result
+Verify page loads
+
+3Step Click “Site Appearance”
+
+Expected Result
+Verify page loads
+
+4Step Click Toggle for Dark mode to ON
+
+Expected Result
+Verify toggle UI change (should change to blue)
+
+5Step Click Save
+
+Expected Result
+Verify UI confirms setting has been saved
+
+6Step Navigate back to home screen
+
+Expected Result
+Verify dark mode is applied
+
+7Step Refresh page
+
+Expected Result
+Verify dark mode is still applied
+
+8Step Repeat for Dark + High contrast mode
+
+Expected Result
+Verify dark + high contrast mode remains applied after saving and refreshing the page
+
+
+ */
